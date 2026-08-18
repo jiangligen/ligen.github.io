f3943baa1509a9473edfaf4c71e6361408fc2388
“jiangligen” <me@ligen.net>
2026-08-18 23:05:13 +0800

    Site updated: 2026-08-18 23:05:13

--- js/busuanzi.pure.mini.js
+++ js/busuanzi.pure.mini.js
@@ -1,1 +1,90 @@
-var bszCaller,bszTag;!function(){var c,d,e,a=!1,b=[];ready=function(c){return a||"interactive"===document.readyState||"complete"===document.readyState?c.call(document):b.push(function(){return c.call(this)}),this},d=function(){for(var a=0,c=b.length;c>a;a++)b[a].apply(document);b=[]},e=function(){a||(a=!0,d.call(window),document.removeEventListener?document.removeEventListener("DOMContentLoaded",e,!1):document.attachEvent&&(document.detachEvent("onreadystatechange",e),window==window.top&&(clearInterval(c),c=null)))},document.addEventListener?document.addEventListener("DOMContentLoaded",e,!1):document.attachEvent&&(document.attachEvent("onreadystatechange",function(){/loaded|complete/.test(document.readyState)&&e()}),window==window.top&&(c=setInterval(function(){try{a||document.documentElement.doScroll("left")}catch(b){return}e()},5)))}(),bszCaller={fetch:function(a,b){var c="BusuanziCallback_"+Math.floor(1099511627776*Math.random());window[c]=this.evalCall(b),a=a.replace("=BusuanziCallback","="+c),scriptTag=document.createElement("SCRIPT"),scriptTag.type="text/javascript",scriptTag.defer=!0,scriptTag.src=a,scriptTag.referrerPolicy="no-referrer-when-downgrade",document.getElementsByTagName("HEAD")[0].appendChild(scriptTag)},evalCall:function(a){return function(b){ready(function(){try{a(b),scriptTag.parentElement.removeChild(scriptTag)}catch(c){bszTag.hides()}})}}},bszCaller.fetch("//busuanzi.ibruce.info/busuanzi?jsonpCallback=BusuanziCallback",function(a){bszTag.texts(a),bszTag.shows()}),bszTag={bszs:["site_pv","page_pv","site_uv"],texts:function(a){this.bszs.map(function(b){var c=document.getElementById("busuanzi_value_"+b);c&&(c.innerHTML=a[b])})},hides:function(){this.bszs.map(function(a){var b=document.getElementById("busuanzi_container_"+a);b&&(b.style.display="none")})},shows:function(){this.bszs.map(function(a){var b=document.getElementById("busuanzi_container_"+a);b&&(b.style.display="inline")})}};
+var bszCaller, bszTag;
+!(function () {
+  var readyCallbacks = [];
+  var isReady = !1;
+
+  function ready(fn) {
+    if (isReady || "interactive" === document.readyState || "complete" === document.readyState) {
+      fn.call(document);
+    } else {
+      readyCallbacks.push(fn);
+    }
+  }
+
+  function runCallbacks() {
+    for (var i = 0; i < readyCallbacks.length; i++) {
+      readyCallbacks[i].apply(document);
+    }
+    readyCallbacks = [];
+  }
+
+  function domReady() {
+    if (!isReady) {
+      isReady = !0;
+      runCallbacks.call(window);
+      if (document.removeEventListener) {
+        document.removeEventListener("DOMContentLoaded", domReady, !1);
+      }
+    }
+  }
+
+  if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", domReady, !1);
+  } else if (document.attachEvent) {
+    document.attachEvent("onreadystatechange", function () {
+      if (/loaded|complete/.test(document.readyState)) domReady();
+    });
+  }
+
+  bszTag = {
+    bszs: ["site_pv", "page_pv", "site_uv", "page_uv"],
+    texts: function (data) {
+      this.bszs.map(function (key) {
+        var el = document.getElementById("busuanzi_value_" + key);
+        if (el && data && data[key] !== undefined) {
+          el.innerHTML = data[key];
+        }
+      });
+    },
+    hides: function () {
+      this.bszs.map(function (key) {
+        var el = document.getElementById("busuanzi_container_" + key);
+        if (el) el.style.display = "none";
+      });
+    },
+    shows: function () {
+      this.bszs.map(function (key) {
+        var el = document.getElementById("busuanzi_container_" + key);
+        if (el) el.style.display = "inline";
+      });
+    }
+  };
+
+  bszCaller = {
+    fetch: function (url, callback) {
+      var targetUrl = "https://cn.vercount.one/api/log?url=" + encodeURIComponent(window.location.href);
+      
+      fetch(targetUrl)
+        .then(function (res) {
+          return res.json();
+        })
+        .then(function (data) {
+          ready(function () {
+            try {
+              callback(data);
+            } catch (err) {
+              bszTag.hides();
+            }
+          });
+        })
+        .catch(function () {
+          bszTag.hides();
+        });
+    }
+  };
+
+  bszCaller.fetch(null, function (data) {
+    bszTag.texts(data);
+    bszTag.shows();
+  });
+})();
